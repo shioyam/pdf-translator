8295d7f421f9f2e790cef2488e84bafa181d96b9
Michihiro Shioya <shioyam@gmail.com>
2025-11-11 12:55:54 +0900

    ファイルアップロード上限を10MBから20MBに変更

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 10 * 1024 * 1024 // 10MB limit
+    fileSize: 20 * 1024 * 1024 // 20MB limit
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'application/pdf') {
